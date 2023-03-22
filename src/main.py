058f69335e08a0a1dbbbf492d396c729fab202d8
Fjodor van Rijsselberg <fjodorvanrijsselberg@Fjodors-MacBook-Pro.local>
2023-03-22 17:14:04 +0100

    Clean up and added new changes to the enhancer objects.

--- src/main.py
+++ src/main.py
@@ -1,10 +1,8 @@
-import requests
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI
 
 from enhancers.KeywordEnhancer import KeywordEnhancer
 from enhancers.VariableEnhancer import VariableEnhancer
 from schema.input import EnhancerInput
-from utils import _try_for_key
 from version import get_version
 
 app = FastAPI()
@@ -19,13 +17,21 @@ async def info():
 @app.post('/dataverse-keyword-enhancer', tags=['Dataverse keyword enhancer'])
 async def dataverse_keyword_enhancer(
         enhancer_input: EnhancerInput) -> dict:
-    keyword_enhancer = KeywordEnhancer(enhancer_input.metadata)
+    keyword_enhancer = KeywordEnhancer(
+        enhancer_input.metadata,
+        'matchElsstTermForKeyword',
+        'https://fuseki.odissei.nl/skosmos/sparql'
+    )
     keyword_enhancer.enhance_metadata()
     return keyword_enhancer.metadata
 
 
 @app.post('/dataverse-variable-enhancer', tags=['Dataverse metadata enhancer'])
 async def dataverse_metadata_enhancer(enhancer_input: EnhancerInput) -> dict:
-    variable_enhancer = VariableEnhancer(enhancer_input.metadata)
+    variable_enhancer = VariableEnhancer(
+        enhancer_input.metadata,
+        'getCbsVarUri',
+        'https://fuseki.odissei.nl/skosmos/sparql'
+    )
     variable_enhancer.enhance_metadata()
     return variable_enhancer.metadata
