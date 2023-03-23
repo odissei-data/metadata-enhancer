0db747103d876de8e1998f6bbe77b032ad5cb157
Thomas van Erven <thomasvanerven@danss-mbp-2.kpn>
2023-03-23 12:54:21 +0100

    Move.

--- src/tests/test_enhancer.py
+++ src/tests/test_enhancer.py
@@ -5,7 +5,7 @@ import sys
 from fastapi import HTTPException
 
 print(sys.path)
-from src.enhancers import KeywordEnhancer, VariableEnhancer
+from ..enhancers import KeywordEnhancer, VariableEnhancer
 
 
 def open_json_file(json_path):
