c2a80bc0495999e57b783b6222ceeb218858b61f
Fjodor van Rijsselberg <fjodorvanrijsselberg@Fjodors-MacBook-Pro.local>
2023-06-29 12:09:14 +0200

    script for table creation that can be used to grab the uris of a label.

--- src/enhancers/queries.py
+++ src/enhancers/queries.py
@@ -1,6 +1,5 @@
 from SPARQLWrapper import SPARQLWrapper, JSON
 
-
 ELSST_VOCAB_QUERY = """
 PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
 PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
@@ -14,8 +13,6 @@ SELECT ?iri ?lbl
 }
 """
 
-
-# Define your SPARQL query
 CBS_VOCAB_QUERY = """
 PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
 PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
@@ -33,17 +30,15 @@ WHERE {
 }
 """
 
-endpoint = "https://fuseki.odissei.nl/skosmos/sparql"
-
 
 def create_table_terms(sparql_endpoint, query):
-    """
-    TODO: Think about possibility of multiple uri's being returned.
-    :param sparql_endpoint:
-    :param query:
-    :return:
-    """
+    """ Creates a dict from vocab with label: uri as the key value.
 
+    TODO: Think about possibility of multiple uri's being returned.
+    :param sparql_endpoint: The endpoint that can be queries with sparql.
+    :param query: The query to fetch the data with.
+    :return: table containing the uri's of the labels in the vocabulary.
+    """
     # Set up the SPARQL query
     sparql = SPARQLWrapper(sparql_endpoint)
     sparql.setReturnFormat(JSON)
@@ -54,8 +49,5 @@ def create_table_terms(sparql_endpoint, query):
         for result in results["results"]["bindings"]:
             table[result["lbl"]["value"]] = result["iri"]["value"]
         return table
-    except:
-        print('Query failed.')
-
-results = create_table_terms(endpoint, ELSST_VOCAB_QUERY)
-print(results)
+    except KeyError:
+        print('Received object does not contain correct keys.')
