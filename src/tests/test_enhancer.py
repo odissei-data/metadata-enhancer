87d1051174b5d3feb76549f4376c2c9028f3fe99
Fjodor van Rijsselberg <fjodorvanrijsselberg@Fjodors-MacBook-Pro.local>
2023-07-25 14:22:09 +0200

    Added tests for existing enrichment block and for unique constraint.

--- src/tests/test_enhancer.py
+++ src/tests/test_enhancer.py
@@ -51,6 +51,12 @@ def cbs_frequency_output():
 
 
 @pytest.fixture()
+def cbs_frequency_existing_block_output():
+    return open_json_file(
+        "test-data/output-data/cbs-frequency-existing-block-output.json")
+
+
+@pytest.fixture()
 def ELSST_enhancer(cbs_metadata, elsst_table):
     return ELSSTEnhancer(
         cbs_metadata,
@@ -74,21 +80,36 @@ def frequency_enhancer(cbs_metadata, frequency_table):
     )
 
 
+@pytest.fixture()
+def frequency_existing_block_enhancer(cbs_keyword_output,
+                                      frequency_table):
+    return FrequencyEnhancer(
+        cbs_keyword_output,
+        frequency_table
+    )
+
+
 def test_e2e_ELSST_enhancer(ELSST_enhancer, cbs_keyword_output):
     ELSST_enhancer.enhance_metadata()
     assert ELSST_enhancer.metadata == cbs_keyword_output
 
 
-def test_e2e_variable_enhancer(variable_enhancer, cbs_metadata,
-                               cbs_variable_output):
+def test_e2e_variable_enhancer(variable_enhancer, cbs_variable_output):
     variable_enhancer.enhance_metadata()
     assert variable_enhancer.metadata == cbs_variable_output
 
 
-def test_e2e_frequency_enhancer(frequency_enhancer, cbs_metadata,
-                                cbs_frequency_output):
+def test_e2e_frequency_enhancer(frequency_enhancer, cbs_frequency_output):
     frequency_enhancer.enhance_metadata()
     assert frequency_enhancer.metadata == cbs_frequency_output
+
+
+def test_existing_enrichments_block_frequency_enhancer(
+        frequency_existing_block_enhancer,
+        cbs_frequency_existing_block_output):
+    frequency_existing_block_enhancer.enhance_metadata()
+    assert frequency_existing_block_enhancer.metadata == \
+           cbs_frequency_existing_block_output
 
 
 def test_get_value_cbs_from_metadata(variable_enhancer, cbs_metadata):
@@ -115,3 +136,19 @@ def test_query_enhancements(variable_enhancer):
     enrichment = variable_enhancer.query_enrichment_table(
         'nonexistent_enhancement')
     assert enrichment is None
+
+
+def test_unique_ELSST_term(ELSST_enhancer):
+    # Call the ELSST_enhance_metadata method twice.
+    ELSST_enhancer.ELSST_enhance_metadata('citation', 'keyword', 'keywordValue')
+    ELSST_enhancer.ELSST_enhance_metadata('citation', 'keyword', 'keywordValue')
+
+    # Check that the 'enrichments' block contains only one 'elsstTerm' with 'Werkgelegenheid'.
+    enrichments_block = ELSST_enhancer.metadata["datasetVersion"]["metadataBlocks"]["enrichments"]
+    elsst_terms = enrichments_block["fields"][0]["value"]
+
+    # There should be only one 'elsstTerm' in the 'enrichments' block
+    assert len(elsst_terms) == 1
+
+    # The term in the 'elsstTerm' should be 'Werkgelegenheid'
+    assert elsst_terms[0]["matchedTerm"]["value"] == "Werkgelegenheid"
