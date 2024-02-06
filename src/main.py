0ac74cfbc81cedd979f9f3327836f22c55c9dde5
Fjodor van Rijsselberg <fjodorvanrijsselberg@Fjodors-MacBook-Pro.local>
2024-02-06 17:17:09 +0100

    Renamed the POST endpoints to be more consistent. Added the CBS concepts and taxonomy endpoints. Added an endpoint that allows for enrichment with any vocabulary. ELSST endpoint requires a language in the path now.

--- src/main.py
+++ src/main.py
@@ -1,30 +1,34 @@
 import os
-
-from fastapi import FastAPI
-
 import terms
 import utils
+from starlette import status
+from fastapi import FastAPI, HTTPException, Path
 from enhancers.FrequencyEnhancer import FrequencyEnhancer
 from enhancers.VocabularyEnhancer import VocabularyEnhancer
 from enhancers.VariableEnhancer import VariableEnhancer
-from queries import create_table_terms, CBS_VOCAB_QUERY, \
-    ELSST_VOCAB_QUERY, create_table_concepts_skosmos
-from schema.input import EnhancerInput
+from api.queries import CBS_VOCAB_QUERY, ELSST_VOCAB_QUERY
+from api.skosmos import create_table_concepts_skosmos
+from api.fuseki import create_table_terms
+from schema.input import EnhancerInput, VocabInput, LangInput, Lang
 from version import get_version
 
 app = FastAPI()
 
 GITHUB_RAW_URL = os.environ['GITHUB_RAW_URL']
-ELSST_VOCABULARY_URL = os.environ['ELSST_VOCABULARY_URL']
-VARIABLE_VOCABULARY_URL = os.environ['VARIABLE_VOCABULARY_URL']
+ELSST_FUSEKI_URL = os.environ['ELSST_FUSEKI_URL']
+VARIABLE_FUSEKI_URL = os.environ['VARIABLE_FUSEKI_URL']
+CBS_VOCAB_URL = os.environ['CBS_VOCAB_URL']
+ELSST_VOCAB_URL = os.environ['ELSST_VOCAB_URL']
 
-CBS_table = create_table_terms(VARIABLE_VOCABULARY_URL, CBS_VOCAB_QUERY)
-ELSST_table = create_table_terms(ELSST_VOCABULARY_URL, ELSST_VOCAB_QUERY)
+CBS_table = create_table_terms(VARIABLE_FUSEKI_URL, CBS_VOCAB_QUERY)
+ELSST_table = create_table_terms(ELSST_FUSEKI_URL, ELSST_VOCAB_QUERY)
 frequency_table = utils.load_tsv_from_github_raw(GITHUB_RAW_URL)
-CBS_taxonomy_table = create_table_concepts_skosmos("https://vocabs.cbs.nl",
-                                                   "taxonomie")
-CBS_vocab_table = create_table_concepts_skosmos("https://vocabs.cbs.nl",
-                                                "begrippen")
+CBS_taxonomy_table = create_table_concepts_skosmos(CBS_VOCAB_URL,
+                                                   "taxonomie", Lang.nl)
+CBS_vocab_table = create_table_concepts_skosmos(CBS_VOCAB_URL,
+                                                "begrippen", Lang.nl)
+ELSST_english_table = create_table_concepts_skosmos(ELSST_VOCAB_URL, "elsst-4",
+                                                    Lang.en)
 
 
 @app.get("/version", tags=["Version"])
@@ -33,21 +37,36 @@ async def info():
     return {"version": result}
 
 
-@app.post('/dataverse-ELSST-enhancer', tags=['Dataverse ELSST enhancer'])
-async def dataverse_ELSST_enhancer(
-        enhancer_input: EnhancerInput) -> dict:
+@app.post('/enrich/elsst/{language}', tags=['Vocabulary enrichment'])
+async def enrich_with_ELSST(
+        enhancer_input: EnhancerInput,
+        language: Lang = Path(
+            ..., title="Language",
+            description="Choose the language for ELSST enrichment")) -> dict:
+
+    if language == 'en':
+        elsst_table = ELSST_english_table
+    elif language == 'nl':
+        elsst_table = ELSST_table
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Invalid language specified, choose between 'en' and 'nl'.",
+        )
+
     ELSST_enhancer = VocabularyEnhancer(
         enhancer_input.metadata,
-        ELSST_table,
+        elsst_table,
         terms.ELSST_terms,
-        terms.ELSST_type_dict
+        "ELSST"
     )
+
     ELSST_enhancer.enhance_metadata()
     return ELSST_enhancer.metadata
 
 
-@app.post('/dataverse-variable-enhancer', tags=['Dataverse variable enhancer'])
-async def dataverse_metadata_enhancer(enhancer_input: EnhancerInput) -> dict:
+@app.post('/enrich/variable', tags=['Variable enrichment'])
+async def enrich_with_variable(enhancer_input: EnhancerInput) -> dict:
     variable_enhancer = VariableEnhancer(
         enhancer_input.metadata,
         CBS_table
@@ -56,9 +75,9 @@ async def dataverse_metadata_enhancer(enhancer_input: EnhancerInput) -> dict:
     return variable_enhancer.metadata
 
 
-@app.post('/dataverse-frequency-enhancer',
-          tags=['Dataverse frequency enhancer'])
-async def dataverse_frequency_enhancer(enhancer_input: EnhancerInput) -> dict:
+@app.post('/enrich/frequency',
+          tags=['Frequency enrichment'])
+async def enrich_with_frequency(enhancer_input: EnhancerInput) -> dict:
     frequency_enhancer = FrequencyEnhancer(
         enhancer_input.metadata,
         frequency_table
@@ -67,27 +86,43 @@ async def dataverse_frequency_enhancer(enhancer_input: EnhancerInput) -> dict:
     return frequency_enhancer.metadata
 
 
-@app.post('/dataverse-cbs-taxonomy-enhancer',
-          tags=['Dataverse CBS taxonomy enhancer'])
-async def dataverse_cbs_taxonomy_enhancer(
-        enhancer_input: EnhancerInput) -> dict:
+@app.post('/enrich/cbs-taxonomy',
+          tags=['Vocabulary enrichment'])
+async def enrich_with_cbs_taxonomy(enhancer_input: EnhancerInput) -> dict:
     taxonomy_enhancer = VocabularyEnhancer(
         enhancer_input.metadata,
         CBS_taxonomy_table,
         terms.CBS_taxonomy_terms,
-        terms.cbs_taxonomy_type_dict
+        "CBS taxonomie"
     )
     taxonomy_enhancer.enhance_metadata()
     return taxonomy_enhancer.metadata
 
 
-@app.post('/dataverse-cbs-vocab-enhancer', tags=['CBS vocab enhancer'])
-async def dataverse_cbs_vocab_enhancer(enhancer_input: EnhancerInput) -> dict:
+@app.post('/enrich/cbs-concepts', tags=['Vocabulary enrichment'])
+async def enrich_with_cbs_concepts(enhancer_input: EnhancerInput) -> dict:
     vocab_enhancer = VocabularyEnhancer(
         enhancer_input.metadata,
         CBS_taxonomy_table,
         terms.CBS_vocab_terms,
-        terms.cbs_vocab_type_dict
+        "CBS begrippen"
+    )
+    vocab_enhancer.enhance_metadata()
+    return vocab_enhancer.metadata
+
+
+@app.post('/enrich/vocabulary', tags=['Vocabulary enrichment'])
+async def enrich_with_vocabulary(vocab_input: VocabInput) -> dict:
+    vocab_table = create_table_concepts_skosmos(
+        vocab_input.endpoint,
+        vocab_input.vocabulary,
+        vocab_input.language
+    )
+    vocab_enhancer = VocabularyEnhancer(
+        vocab_input.metadata,
+        vocab_table,
+        vocab_input.terms,
+        vocab_input.vocabulary_name,
     )
     vocab_enhancer.enhance_metadata()
     return vocab_enhancer.metadata
--- src/schema/input.py
+++ src/schema/input.py
@@ -1,7 +1,26 @@
+from enum import Enum
 from typing import Any
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class EnhancerInput(BaseModel):
     metadata: list | dict | Any
+
+
+class VocabInput(BaseModel):
+    metadata: list | dict | Any
+    endpoint: str = Field(example="https://vocabs.cbs.nl")
+    vocabulary: str = Field(example="begrippen")
+    vocabulary_name: str = Field(example="CBS concepts")
+    language: str = Field(example="nl")
+    terms: list = Field(example="keyword")
+
+
+class Lang(str, Enum):
+    en = "en"
+    nl = "nl"
+
+
+class LangInput(BaseModel):
+    language: Lang
